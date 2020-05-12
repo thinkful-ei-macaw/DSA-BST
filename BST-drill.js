e9de5d3065a2e464cdfc00d2b9ccfa43161f5991
Brannen Petit <bpetit940@gmail.com>
2020-05-12 14:56:14 -0500

    working on 7

--- BST-drill.js
+++ BST-drill.js
@@ -1,29 +1,77 @@
 /* eslint-disable */
 
-const BinarySearchTree = require('./BST');
+const BinarySearchTree = require("./BST");
 
-function tree(t){
-    if(!t){
-        return 0;
-    }
-    return tree(t.left) + t.value + tree(t.right)
+function tree(t) {
+  if (!t) {
+    return 0;
+  }
+  return tree(t.left) + t.value + tree(t.right);
+}
+function treeLarge(t) {
+  let first = [];
+
+  if (!t) {
+    return 0;
+  }
+
+  if (!t.right) {
+    return console.log(first);
+  }
+  first.push(t.right.value);
+  treeLarge(t.right);
+  return "something";
+}
+function heightTrak(bst) {
+  if (!bst) {
+    return 0;
+  }
+
+  let leftHeight = heightTrak(bst.left);
+  let rightHeigth = heightTrak(bst.right);
+  return Math.max(leftHeight, rightHeigth) + 1;
 }
 
+function isBST(bst) {
+  if (!bst) {
+    return false;
+  }
+  let leftHeight = heightTrak(bst.left);
+  let rightHeigth = heightTrak(bst.right);
+  let difference = leftHeight - rightHeigth;
+  console.log(difference);
+  if (difference <= 1 && difference >= -1) {
+    return true;
+  }
+
+  return false;
+
+  // if(!bst.left || !bst.right) {
+
+  // }
+  //   if (bst.left.value > bst.value) {
+  //     return false;
+  //   }
+  //   if (bst.right.value < bst.value) {
+  //     return false;
+  //   }
+}
+
+function thirdLarge(bst) {}
+
 function main() {
-    const BST = new BinarySearchTree();
-    BST.insert(3);
-    BST.insert(1);
-    BST.insert(4);
-    BST.insert(6);
-    BST.insert(9);
-    BST.insert(2);
-    BST.insert(5);
-    BST.insert(7);
+  const BST = new BinarySearchTree();
+  BST.insert(3);
+  BST.insert(1);
+  BST.insert(4);
+  BST.insert(6);
+  BST.insert(9);
+  BST.insert(2);
+  BST.insert(5);
 
+  console.log(treeLarge(BST));
 
-    console.log(tree(BST));
-    
-    // console.log(BST);
+  // console.log(BST);
 }
 
 main();
